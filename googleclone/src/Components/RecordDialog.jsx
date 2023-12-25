fe3c7b945da1213d4be51485db0f4eefb0183a23
Mjawad12 <Workingofficial156@gmail.com>
2023-12-25 19:15:26 +0500

    added speech recoginition

---
+++ googleclone/src/Components/RecordDialog.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect } from "react";
+
+export default function RecordDialog({
+  setrecordDialog,
+  recordDialog,
+  search,
+}) {
+  useEffect(() => {
+    if (recordDialog) {
+      const SpeechRecognition =
+        window.speechRecognition || window.webkitSpeechRecognition;
+      const recognition = new SpeechRecognition();
+      recognition.interimResults = true;
+      recognition.addEventListener("result", async (e) => {
+        const transScript = Array.from(e.results)
+          .map((result) => result[0])
+          .map((result) => result.transcript)
+          .join("");
+        const ptag = document.querySelector(".values");
+        ptag.innerText = ptag && transScript;
+        if (e.results[0].isFinal) {
+          setrecordDialog(false);
+          document.body.classList.remove("no-scroll");
+          search(transScript);
+          document.querySelector(".text").value = transScript;
+        }
+      });
+      recognition.start();
+    }
+    // eslint-disable-next-line
+  }, []);
+  const abortRec = () => {
+    setrecordDialog(false);
+    document.body.classList.remove("no-scroll");
+  };
+  return (
+    <div className="rDWrapper">
+      <svg
+        onClick={() => {
+          setrecordDialog(false);
+        }}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 21 24"
+        height={"40"}
+        width={"40"}
+        className="crossSVG"
+        fill=" rgba(0, 0, 0, 0.65)"
+      >
+        <path d="M19,6.41L17.59,5,12,10.59,6.41,5,5,6.41,10.59,12,5,17.59,6.41,19,12,13.41,17.59,19,19,17.59,13.41,12,19,6.41Z" />
+      </svg>
+      <div className="rD">
+        <p className="values">Speak Now</p>
+        <div className="micr">
+          <svg
+            version="1.0"
+            xmlns="http://www.w3.org/2000/svg"
+            width="96.000000pt"
+            height="96.000000pt"
+            viewBox="0 0 96.000000 96.000000"
+            preserveAspectRatio="xMidYMid meet"
+            fill="red"
+            onClick={abortRec}
+          >
+            <g
+              transform="translate(0.000000,96.000000) scale(0.100000,-0.100000)"
+              fill="red"
+              stroke="none"
+            >
+              <path
+                d="M413 830 c-57 -34 -65 -64 -61 -220 3 -138 3 -139 32 -169 36 -38 84
+-53 129 -40 18 5 46 23 63 40 29 30 29 31 32 169 3 112 0 144 -13 170 -20 40
+-70 70 -115 70 -19 0 -49 -9 -67 -20z"
+              />
+              <path
+                d="M247 496 c-6 -16 5 -73 21 -102 21 -39 92 -94 137 -106 l45 -12 0
+-48 0 -48 -49 0 c-62 0 -81 -7 -81 -30 0 -26 19 -30 160 -30 141 0 160 4 160
+30 0 23 -19 30 -81 30 l-49 0 0 48 0 48 45 12 c45 12 116 67 137 106 16 29 27
+86 21 102 -9 25 -42 16 -49 -13 -16 -74 -52 -121 -107 -142 -36 -14 -118 -14
+-154 0 -55 21 -91 68 -107 142 -7 29 -40 38 -49 13z"
+              />
+            </g>
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+}
--- googleclone/src/Components/Search.jsx
+++ googleclone/src/Components/Search.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import RecordDialog from "./RecordDialog";
 
 export default function Search(props) {
   const [searchHistory, setsearchHistory] = useState(
@@ -11,6 +12,7 @@ export default function Search(props) {
   const inputRef = useRef("");
   const hist = useRef("");
   const [cross, setcross] = useState(false);
+  const [recordDialog, setrecordDialog] = useState(false);
   const handleKey = (e) => {
     inputRef.current.value !== "" ? setcross(true) : setcross(false);
     if (e.key === "Enter") {
@@ -75,145 +77,184 @@ export default function Search(props) {
   };
 
   return (
-    <div
-      ref={inputMainRef}
-      onClick={() => {
-        inputRef.current.focus();
-      }}
-      onBlur={(e) => {
-        setcross(e.target.value !== "");
-      }}
-      style={{ borderRadius: clicked && "23px" }}
-      className="iner inputDiv"
-    >
-      <div className="iner inputPrimary">
-        <svg
-          focusable="false"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          height={"21"}
-          width={"21"}
-          fill=" rgba(0, 0, 0, 0.35)"
-          className=" iner sear"
-        >
-          <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
-        </svg>
-        <input
-          ref={inputRef}
-          onFocus={(e) => {
-            setclicked(true);
-          }}
-          onKeyDown={handleKey}
-          className=" iner text"
-        ></input>
-        {cross && (
+    <>
+      {recordDialog && (
+        <RecordDialog
+          setrecordDialog={setrecordDialog}
+          recordDialog={recordDialog}
+          search={search}
+        />
+      )}
+      <div
+        ref={inputMainRef}
+        onClick={() => {
+          inputRef.current.focus();
+        }}
+        onBlur={(e) => {
+          setcross(e.target.value !== "");
+        }}
+        style={{ borderRadius: clicked && "23px" }}
+        className="iner inputDiv"
+      >
+        <div className="iner inputPrimary">
           <svg
-            onClick={() => {
-              inputRef.current.value = "";
-              setcross(false);
-            }}
+            focusable="false"
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 21 24"
-            height={"24"}
-            width={"24"}
-            fill=" rgba(0, 0, 0, 0.65)"
+            viewBox="0 0 24 24"
+            height={"21"}
+            width={"21"}
+            fill=" rgba(0, 0, 0, 0.35)"
+            className=" iner sear"
           >
-            <path d="M19,6.41L17.59,5,12,10.59,6.41,5,5,6.41,10.59,12,5,17.59,6.41,19,12,13.41,17.59,19,19,17.59,13.41,12,19,6.41Z" />
+            <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
           </svg>
-        )}
-      </div>
-      {clicked && (
-        <>
-          <div ref={hist} className=" iner histDiv">
-            <hr />
-            {searchHistory &&
-              searchHistory.slice(0, 8).map((it, index) => {
-                return (
-                  <span key={index} className="iner his">
-                    <div
-                      className="sp"
-                      searchvalue={it}
-                      onClick={handlePreviousSerach}
-                    >
-                      <svg
-                        height="20"
-                        width="20"
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="iner"
+          <input
+            ref={inputRef}
+            onFocus={(e) => {
+              setclicked(true);
+            }}
+            onKeyDown={handleKey}
+            className=" iner text"
+          ></input>
+          {cross && (
+            <svg
+              onClick={() => {
+                inputRef.current.value = "";
+                setcross(false);
+              }}
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 21 24"
+              height={"24"}
+              width={"24"}
+              fill=" rgba(0, 0, 0, 0.65)"
+            >
+              <path d="M19,6.41L17.59,5,12,10.59,6.41,5,5,6.41,10.59,12,5,17.59,6.41,19,12,13.41,17.59,19,19,17.59,13.41,12,19,6.41Z" />
+            </svg>
+          )}
+          <div
+            className="iner"
+            onClick={() => {
+              setrecordDialog(true);
+              document.body.classList.add("no-scroll");
+            }}
+          >
+            <svg
+              className="goxjub"
+              focusable="false"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              height={"25px"}
+              width={"30px"}
+            >
+              <path
+                fill="#4285f4"
+                d="m12 15c1.66 0 3-1.31 3-2.97v-7.02c0-1.66-1.34-3.01-3-3.01s-3 1.34-3 3.01v7.02c0 1.66 1.34 2.97 3 2.97z"
+              ></path>
+              <path fill="#34a853" d="m11 18.08h2v3.92h-2z"></path>
+              <path
+                fill="#fbbc05"
+                d="m7.05 16.87c-1.27-1.33-2.05-2.83-2.05-4.87h2c0 1.45 0.56 2.42 1.47 3.38v0.32l-1.15 1.18z"
+              ></path>
+              <path
+                fill="#ea4335"
+                d="m12 16.93a4.97 5.25 0 0 1 -3.54 -1.55l-1.41 1.49c1.26 1.34 3.02 2.13 4.95 2.13 3.87 0 6.99-2.92 6.99-7h-1.99c0 2.92-2.24 4.93-5 4.93z"
+              ></path>
+            </svg>
+          </div>
+        </div>
+        {clicked && (
+          <>
+            <div ref={hist} className=" iner histDiv">
+              <hr />
+              {searchHistory &&
+                searchHistory.slice(0, 8).map((it, index) => {
+                  return (
+                    <span key={index} className="iner his">
+                      <div
+                        className="sp"
+                        searchvalue={it}
+                        onClick={handlePreviousSerach}
                       >
-                        <circle
-                          cx="10"
-                          cy="9"
-                          r="8"
-                          stroke="rgba(0, 0, 0, 0.300)"
-                          strokeWidth="1.7"
-                          fill="white"
-                          pointerEvents={"none"}
-                        />
-                        <line
-                          x1="10"
-                          y1="10"
-                          x2="10"
-                          y2="5"
-                          stroke="rgba(0, 0, 0, 0.368)"
-                          strokeWidth="1"
-                          transform="rotate(120 10 10)"
-                          pointerEvents={"none"}
-                        />
-                        <line
-                          x1="10"
-                          y1="10.1"
-                          x2="10"
-                          y2="3"
-                          stroke="rgba(0, 0, 0, 0.368)"
-                          strokeWidth="1"
-                          transform="rotate(0 10 10)"
-                          pointerEvents={"none"}
+                        <svg
+                          height="20"
+                          width="20"
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="iner"
+                        >
+                          <circle
+                            cx="10"
+                            cy="9"
+                            r="8"
+                            stroke="rgba(0, 0, 0, 0.300)"
+                            strokeWidth="1.7"
+                            fill="white"
+                            pointerEvents={"none"}
+                          />
+                          <line
+                            x1="10"
+                            y1="10"
+                            x2="10"
+                            y2="5"
+                            stroke="rgba(0, 0, 0, 0.368)"
+                            strokeWidth="1"
+                            transform="rotate(120 10 10)"
+                            pointerEvents={"none"}
+                          />
+                          <line
+                            x1="10"
+                            y1="10.1"
+                            x2="10"
+                            y2="3"
+                            stroke="rgba(0, 0, 0, 0.368)"
+                            strokeWidth="1"
+                            transform="rotate(0 10 10)"
+                            pointerEvents={"none"}
+                          />
+                        </svg>
+                        {it}
+                      </div>
+                      <svg
+                        onClick={removeHist}
+                        index={index}
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 21 24"
+                        height={"24"}
+                        width={"24"}
+                        className="cross_ser"
+                        fill="rgba(0, 0, 0, 0.300)"
+                      >
+                        <path
+                          style={{ pointerEvents: "none" }}
+                          d="M19,6.41L17.59,5,12,10.59,6.41,5,5,6.41,10.59,12,5,17.59,6.41,19,12,13.41,17.59,19,19,17.59,13.41,12,19,6.41Z"
                         />
                       </svg>
-                      {it}
-                    </div>
-                    <svg
-                      onClick={removeHist}
-                      index={index}
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 21 24"
-                      height={"24"}
-                      width={"24"}
-                      className="cross_ser"
-                      fill="rgba(0, 0, 0, 0.300)"
-                    >
-                      <path
-                        style={{ pointerEvents: "none" }}
-                        d="M19,6.41L17.59,5,12,10.59,6.41,5,5,6.41,10.59,12,5,17.59,6.41,19,12,13.41,17.59,19,19,17.59,13.41,12,19,6.41Z"
-                      />
-                    </svg>
-                  </span>
-                );
-              })}
-            {clicked && !res && (
-              <div style={{ marginTop: "1.5rem" }} className="btn-div">
-                <button
-                  onClick={() => {
-                    search(inputRef.current.value);
-                  }}
-                  className=" btn-p"
-                >
-                  Google Search
-                </button>
-                <a
-                  href="https://doodles.google/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="iner btn-p"
-                >
-                  I'm Feeling Lucky
-                </a>
-              </div>
-            )}
-          </div>
-        </>
-      )}
-    </div>
+                    </span>
+                  );
+                })}
+              {clicked && !res && (
+                <div style={{ marginTop: "1.5rem" }} className="btn-div">
+                  <button
+                    onClick={() => {
+                      search(inputRef.current.value);
+                    }}
+                    className=" btn-p"
+                  >
+                    Google Search
+                  </button>
+                  <a
+                    href="https://doodles.google/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="iner btn-p"
+                  >
+                    I'm Feeling Lucky
+                  </a>
+                </div>
+              )}
+            </div>
+          </>
+        )}
+      </div>
+    </>
   );
 }
